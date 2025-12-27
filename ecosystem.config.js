ba02838db13c34408ca360eb438e7f26dbcf8680
Liran Tal <liran.tal@gmail.com>
2025-12-27 12:51:32 +0000

    fix: pm2 container setup to avoid using caddy by default (ssl only works on the host)

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,15 +19,16 @@ module.exports = {
       autorestart: false,
       exec_mode: 'fork',
     },
-    {
-      name: "caddy",
-      cwd: "./infra/http-server",  // Path to Caddy configuration
-      script: "caddy",
-      args: "run --config Caddyfile",   // Runs Caddy with the Caddyfile
-      interpreter: "none",
-      autorestart: false,
-      exec_mode: 'fork',
-    }
+    // only enable when running Caddy http proxy in the container
+    // {
+    //   name: "caddy",
+    //   cwd: "./infra/http-server",  // Path to Caddy configuration
+    //   script: "caddy",
+    //   args: "run --config Caddyfile",   // Runs Caddy with the Caddyfile
+    //   interpreter: "none",
+    //   autorestart: false,
+    //   exec_mode: 'fork',
+    // }
   ]
 };
 
